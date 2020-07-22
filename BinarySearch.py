7d065ad51105208fc8c0ef81cc33673987d957b2
Omri <omrii229@gmail.com>
2020-07-22 18:51:27 +0300

    Added random option

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,10 +1,15 @@
 __version__=0.1
 __author="Omri"
 
+import random
+
+FROM=1
+TO=100
+ELEMENTS=5
 def get_input():
 	A=input("Enter numbers:\n").split(' ')
 	A=[n for n in A if n.isdigit()]
-	return sorted(A)
+	return list(map(int,sorted(A)))
 	
 def bin_search(A,num,high,low):
 	if high>=low:
@@ -18,13 +23,24 @@ def bin_search(A,num,high,low):
 	else:
 		return -1
 	
+def random_generate():
+	A = random.sample(range(FROM, TO), ELEMENTS)
+	return sorted(A)
 
 def main():
-	A=get_input()
+	choice=''
+	while not choice.isdigit():
+		choice=input("1 - Insert manually numbers , 2 - Random:\n")
+		if choice.isdigit() and int(choice) not in range (1,3):
+			choice=''
+	if choice == "1":
+		A=get_input()
+	else:
+		A=random_generate()
 	num=""
-	while(not num.isdigit()):
+	while not num.isdigit():
 		num=input("What number to search? ")
-	index=bin_search(A,num,len(A)-1,0)
+	index=bin_search(A,int(num),len(A)-1,0)
 	if index != -1:
 		print("Found at index {0}".format(index))
 	else:
